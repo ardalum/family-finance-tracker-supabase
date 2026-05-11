eb90ecf990456c9dc93952b2c87860c24ebe7344
Arvin <ardalumpines@outlook.com>
2026-05-11 16:46:10 -0400

    prepare walletflow for github pages deployement

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/family-finance-tracker-supabase/',
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
