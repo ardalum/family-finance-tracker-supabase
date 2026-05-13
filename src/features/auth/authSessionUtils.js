43eb40de125bf5f653e259af996eb527787a29ae
Arvin <47518071+ardalum@users.noreply.github.com>
2026-05-13 16:41:01 -0400

    Add auth session utility exports

---
+++ src/features/auth/authSessionUtils.js
@@ -0,0 +1,12 @@
+export {
+  SESSION_CHECK_INTERVAL_MS,
+  SESSION_WARNING_THRESHOLD_MS,
+} from "./authSessionConfig.js";
+
+export {
+  formatRemainingTime,
+  getSessionExpiryMs,
+  getSessionState,
+  getSessionSummary,
+  getSessionTimeRemainingMs,
+} from "./accountDisplayUtils.js";
