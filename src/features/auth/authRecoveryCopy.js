3e9b45b6c94b7dd7ec1d5c2907976638d020e2f9
Arvin <47518071+ardalum@users.noreply.github.com>
2026-05-13 23:58:13 -0400

    Add recovery form copy constants

---
+++ src/features/auth/authRecoveryCopy.js
@@ -0,0 +1,20 @@
+export const AUTH_RECOVERY_FORM_COPY = {
+  brandDescription: "Create a new password for your account.",
+  title: "Reset your password",
+  description: "Enter a new password below. You will use this password the next time you sign in.",
+  passwordLabel: "New password",
+  confirmPasswordLabel: "Confirm new password",
+  submitLabel: "Continue",
+};
+
+export const AUTH_RECOVERY_FORM_STATUS_COPY = {
+  placeholderSuccess: "Password update UI is ready. The secure update service will be connected in the next PR.",
+};
+
+export function getRecoveryPasswordToggleLabel(isVisible) {
+  return isVisible ? "Hide new password" : "Show new password";
+}
+
+export function getRecoveryConfirmPasswordToggleLabel(isVisible) {
+  return isVisible ? "Hide confirm password" : "Show confirm password";
+}
