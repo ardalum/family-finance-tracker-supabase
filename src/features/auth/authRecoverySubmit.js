146c9fd1b45bb02afac81ccb0aa67ede24fc5887
Arvin <47518071+ardalum@users.noreply.github.com>
2026-05-14 00:48:43 -0400

    Normalize recovery submit value (#126)

--- src/features/auth/authRecoverySubmit.js
+++ src/features/auth/authRecoverySubmit.js
@@ -1,7 +1,9 @@
 import { AUTH_RECOVERY_FORM_STATUS_COPY } from "./authRecoveryCopy.js";
+import { getNormalizedRecoveryFormValues } from "./authRecoveryFormState.js";
 
 export async function submitRecoveryForm({ password } = {}) {
-  void password;
+  const { primaryValue } = getNormalizedRecoveryFormValues({ primaryValue: password });
+  void primaryValue;
 
   return {
     status: AUTH_RECOVERY_FORM_STATUS_COPY.placeholderSuccess,
