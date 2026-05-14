09cf731752b990469da7e4d6933765c3ed38989d
Arvin <47518071+ardalum@users.noreply.github.com>
2026-05-14 01:12:43 -0400

    Wire recovery submit to auth user update (#134)

--- src/features/auth/authRecoverySubmit.js
+++ src/features/auth/authRecoverySubmit.js
@@ -1,5 +1,6 @@
 import { AUTH_RECOVERY_FORM_STATUS_COPY } from "./authRecoveryCopy.js";
 import { getNormalizedRecoveryFormValues } from "./authRecoveryFormState.js";
+import { updateAuthUser } from "./authService.js";
 import { replaceAuthStatusUrl } from "./authStatusUtils.js";
 
 export function createRecoverySubmitResult({ status = "" } = {}) {
@@ -12,8 +13,8 @@ export function completeRecoveryUrlStep() {
 
 export async function submitRecoveryForm({ password } = {}) {
   const { primaryValue } = getNormalizedRecoveryFormValues({ primaryValue: password });
-  void primaryValue;
 
+  await updateAuthUser({ password: primaryValue });
   completeRecoveryUrlStep();
 
   return createRecoverySubmitResult({
