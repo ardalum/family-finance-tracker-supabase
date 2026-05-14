be8d8be4c8477f8f4a9e483cf5b26b8382fdb7a5
Arvin <47518071+ardalum@users.noreply.github.com>
2026-05-14 00:15:48 -0400

    Pass recovery value to submit helper (#111)
    * Accept recovery submit values
    
    * Pass recovery value to submit helper

--- src/features/auth/authRecoverySubmit.js
+++ src/features/auth/authRecoverySubmit.js
@@ -1,6 +1,8 @@
 import { AUTH_RECOVERY_FORM_STATUS_COPY } from "./authRecoveryCopy.js";
 
-export async function submitRecoveryForm() {
+export async function submitRecoveryForm({ password } = {}) {
+  void password;
+
   return {
     status: AUTH_RECOVERY_FORM_STATUS_COPY.placeholderSuccess,
   };
