c0f32dec32c9297bf40bfe3ab013d4417a2ce8d7
Arvin <47518071+ardalum@users.noreply.github.com>
2026-05-15 00:38:30 -0400

    Add refresh data utility helpers

---
+++ src/app/refreshDataUtils.js
@@ -0,0 +1,63 @@
+export async function runRefreshSequence(refreshCallbacks = []) {
+  for (const refreshCallback of refreshCallbacks) {
+    await refreshCallback();
+  }
+}
+
+export function createDashboardInsightsRefreshers({ loadDashboardData, loadInsightsData } = {}) {
+  return [loadDashboardData, loadInsightsData].filter(Boolean);
+}
+
+export function createSpendingDashboardInsightsRefreshers({
+  loadSpendingTransactions,
+  loadDashboardData,
+  loadInsightsData,
+} = {}) {
+  return [loadSpendingTransactions, loadDashboardData, loadInsightsData].filter(Boolean);
+}
+
+export function createRecurringDashboardInsightsRefreshers({
+  loadRecurringData,
+  loadDashboardData,
+  loadInsightsData,
+} = {}) {
+  return [loadRecurringData, loadDashboardData, loadInsightsData].filter(Boolean);
+}
+
+export function createRecurringSpendingDashboardInsightsRefreshers({
+  loadRecurringData,
+  loadSpendingTransactions,
+  loadDashboardData,
+  loadInsightsData,
+} = {}) {
+  return [
+    loadRecurringData,
+    loadSpendingTransactions,
+    loadDashboardData,
+    loadInsightsData,
+  ].filter(Boolean);
+}
+
+export function createAllSupabaseRefreshers({
+  loadSupabaseCreditCards,
+  loadSupabaseMonthlyBalances,
+  loadSupabaseBudgets,
+  loadSpendingCategories,
+  loadSpendingTransactions,
+  loadDashboardData,
+  loadInsightsData,
+  loadRecurringCategories,
+  loadRecurringData,
+} = {}) {
+  return [
+    loadSupabaseCreditCards,
+    loadSupabaseMonthlyBalances,
+    loadSupabaseBudgets,
+    loadSpendingCategories,
+    loadSpendingTransactions,
+    loadDashboardData,
+    loadInsightsData,
+    loadRecurringCategories,
+    loadRecurringData,
+  ].filter(Boolean);
+}
