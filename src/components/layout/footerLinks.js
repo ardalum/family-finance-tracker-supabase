c78bdfc5971b3add0a068f7571a41c7a5e9ac079
Arvin <47518071+ardalum@users.noreply.github.com>
2026-05-16 19:48:17 -0400

    Extract footer links

---
+++ src/components/layout/footerLinks.js
@@ -0,0 +1,7 @@
+export const footerLinks = [
+  { targetView: "privacy-policy", label: "Privacy" },
+  { targetView: "terms-of-use", label: "Terms" },
+  { targetView: "help-support", label: "Help" },
+  { targetView: "release-notes", label: "Release Notes" },
+  { targetView: "about", label: "About" },
+];
