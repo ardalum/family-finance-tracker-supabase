5418b4f9fd21f8259eb96d34f1ca06cf6485f8fe
Arvin <47518071+ardalum@users.noreply.github.com>
2026-05-15 21:30:49 -0400

    Dedupe app data composition helper

--- src/app/appDataComposition.js
+++ src/app/appDataComposition.js
@@ -1,4 +1,4 @@
-export function createDashboardAppData({
+export function createFeatureAppData({
   appData,
   creditCards,
   monthlyBalances,
@@ -23,27 +23,10 @@ export function createDashboardAppData({
   };
 }
 
-export function createInsightsAppData({
-  appData,
-  creditCards,
-  monthlyBalances,
-  selectedMonth,
-  budgets,
-  transactions,
-  recurringPayments,
-  recurringStatusByMonth,
-} = {}) {
-  return {
-    ...appData,
-    creditCards,
-    monthlyBalances,
-    budgetsByMonth: {
-      ...appData?.budgetsByMonth,
-      [selectedMonth]: budgets,
-    },
-    transactions,
-    recurringPayments,
-    recurringStatusByMonth,
-    recurringTransactions: transactions,
-  };
+export function createDashboardAppData(input = {}) {
+  return createFeatureAppData(input);
+}
+
+export function createInsightsAppData(input = {}) {
+  return createFeatureAppData(input);
 }
