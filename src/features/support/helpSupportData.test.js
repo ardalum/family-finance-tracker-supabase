7a404dee64e43396f194e35f0b8df5b623a7501a
Arvin <47518071+ardalum@users.noreply.github.com>
2026-05-19 23:48:32 -0400

    Apply Prettier formatting to help support test

--- src/features/support/helpSupportData.test.js
+++ src/features/support/helpSupportData.test.js
@@ -58,6 +58,8 @@ describe("help/support trust copy", () => {
 
   it("includes practical troubleshooting steps", () => {
     assert.ok(troubleshootingChecklist.length >= 5);
-    assert.ok(troubleshootingChecklist.some((item) => /paid-from account/i.test(item)));
+    assert.ok(
+      troubleshootingChecklist.some((item) => /paid-from account/i.test(item)),
+    );
   });
 });
